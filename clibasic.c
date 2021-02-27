ca8028fa71d76e88dbd70af13ba5a0daf39d3a45
PQCraft <58464017+PQCraft@users.noreply.github.com>
2021-02-27 04:11:39 -0500

    0.4.5
    Reduced size from 19.6kb to 18.8kb

--- clibasic.c
+++ clibasic.c
@@ -10,7 +10,7 @@
 #include <sys/time.h>
 #include <quadmath.h>
 
-char VER[] = "0.4.4";
+char VER[] = "0.4.5";
 
 FILE **f;
 
@@ -90,11 +90,6 @@ int main(int argc, char *argv[]) {
     printf("Command Line Interface BASIC version %s\n", VER);
     if (debug) printf("Running in debug mode\n");
     char conbuf[32768];
-    //cmd = malloc(0);
-    //argl = malloc(0);
-    //arg = malloc(0);
-    //tmpargs = malloc(0);
-    //argt = malloc(0);
     while (!exit) {
         for (int i = 0; i < 32768; i++) conbuf[i] = 0;
         int cp = 0;
@@ -140,8 +135,6 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-// reserved for mkvar and rmvar define lines
-
 bool isSpChar(char* bfr, int pos) {
     return (bfr[pos] == '+' || bfr[pos] == '-' || bfr[pos] == '*' || bfr[pos] == '/' || bfr[pos] == '^' || bfr[pos] == ',');
 }
@@ -168,7 +161,6 @@ void copyStrTo(char* str1, int i, char* str2) {
 
 void copyStrApnd(char* str1, char* str2) {
     int j = 0, i = 0;
-    //if (debug) printf("copyStrApnd: strlen(str2): [%d]\n", (int)strlen(str2));
     for (i = strlen(str2); str1[j] != '\0'; i++) {str2[i] = str1[j]; j++;}
     str2[i] = 0;
 }
@@ -251,7 +243,6 @@ uint8_t getVal(char* inbuf, char* outbuf) {
     if (debug) printf("checking for syntax error\n");
     if (isSpChar(inbuf, 0) || isSpChar(inbuf, strlen(inbuf) - 1)) {cerr = 1; return 0;}
     if (debug) printf("no syntax error detected\n");
-    outbuf[0] = 0; //REM (427689): REMOVE LATER
     tmp[0][0] = 0; tmp[1][0] = 0;
     while (true) {
         if (debug) printf("getVal (1): ip: [%d], jp: [%d]\n", ip, jp);
@@ -336,13 +327,9 @@ uint8_t getVal(char* inbuf, char* outbuf) {
                 if (tmp[2][strlen(tmp[2]) - 1] == '.') {tmp[2][strlen(tmp[2]) - 1] = 0;}
                 if (debug) printf("getVal: tmp[0]: {%s}, tmp[1]: {%s}, tmp[2]: {%s}, tmp[3]: {%s}\n", tmp[0], tmp[1], tmp[2], tmp[3]);
                 copyStrSnip(tmp[0], p3, strlen(tmp[0]), tmp[3]);
-                if (debug) printf("getVal: tmp[0]: {%s}, tmp[1]: {%s}, tmp[2]: {%s}, tmp[3]: {%s}\n", tmp[0], tmp[1], tmp[2], tmp[3]);
                 if (p1 != 0) copyStrSnip(tmp[0], 0, p1 + 1, tmp[1]);
-                if (debug) printf("getVal: tmp[0]: {%s}, tmp[1]: {%s}, tmp[2]: {%s}, tmp[3]: {%s}\n", tmp[0], tmp[1], tmp[2], tmp[3]);
                 copyStrApnd(tmp[2], tmp[1]);
-                if (debug) printf("getVal: tmp[0]: {%s}, tmp[1]: {%s}, tmp[2]: {%s}, tmp[3]: {%s}\n", tmp[0], tmp[1], tmp[2], tmp[3]);
                 copyStrApnd(tmp[3], tmp[1]);
-                if (debug) printf("getVal: tmp[0]: {%s}, tmp[1]: {%s}, tmp[2]: {%s}, tmp[3]: {%s}\n", tmp[0], tmp[1], tmp[2], tmp[3]);
                 copyStr(tmp[1], tmp[0]);
                 if (debug) printf("getVal: tmp[0]: {%s}, tmp[1]: {%s}, tmp[2]: {%s}, tmp[3]: {%s}\n", tmp[0], tmp[1], tmp[2], tmp[3]);
             }
@@ -406,8 +393,7 @@ bool mkargs() {
                 else {argl[0]++;}
             } else {
                 if (argct == 0) argct = 1;
-                //while (cmd[cmdpos] == ' ' && !inStr/* && (is_spCharS1(cmd, cmdpos - 1) || (is_spCharS1(cmd, cmdpos + 1) && is_spCharS1(cmd, cmdpos - 1)))*/) {cmdpos++;}
-                if (cmd[cmdpos] == ' ' && !inStr) {argl[i]--;}// else {if (debug) printf("?: %d\n", argl[i]);}
+                if (cmd[cmdpos] == ' ' && !inStr) {argl[i]--;}
                 if (cmd[cmdpos] == ',' && !inStr && pct == 0) {argct++; argl = realloc(argl, (argct + 1) * sizeof(int)); goto exitctloop;} else
                 if (cmd[cmdpos] == '"') {inStr = !inStr;} else
                 if (cmd[cmdpos] == '(' && !inStr) {pct++;} else
@@ -431,12 +417,11 @@ bool mkargs() {
     if (debug) printf("argct: %d\n", argct);
     tmpargs = realloc(tmpargs, (argct + 1) * sizeof(char*));
     if (debug) printf("allocate tmpargs size:\n");
-    for (int i = 0; i <= argct; i++) {if (debug) {printf("realloc size [%d]: %d\n", i, argl[i] + 1);}}
+    if (debug) {for (int i = 0; i <= argct; i++) {printf("realloc size [%d]: %d\n", i, argl[i] + 1);}}
     if (debug) printf("allocating tmpargs:\n");
     for (int i = 0; i <= argct; i++) {
         if (debug) printf("realloc size [%d]: %d\n", i, argl[i] + 1);
         tmpargs[i] = (char*)realloc(tmpargs[i], (argl[i] + 1) * sizeof(char));
-        if (debug) printf("done\n");
     }
     if (debug) printf("put args in arg containers:\n");
     for (int i = 0; i <= argct; i++) {
@@ -457,10 +442,8 @@ bool mkargs() {
                 tmpargs[i][argpos] = cmd[cmdpos];
                 argpos++;
             }
-            //if (cmd[cmdpos] == '\0') goto fexittxloop;
             cmdpos++;
             if (debug) printf("2t[%d], [%d]: %c (%d)\n", i, cmdpos, cmd[cmdpos], (int)cmd[cmdpos]);
-            //if (cmd[cmdpos] == '\0') goto fexittxloop;
         }
         tmpargs[i][argl[i]] = '\0';
         exittxloop:
@@ -471,7 +454,6 @@ bool mkargs() {
         if (debug) printf("3t[%d], [%d]: %c [%d]\n", i, cmdpos, cmd[cmdpos], argct);
         if (debug) printf("tmpargs[%d]: {%s}\n", i, tmpargs[i]);
     }
-    //fexittxloop:
     if (debug) printf("argct: %d\n", argct);    
     if (debug) printf("check for blank arg:\n");
     if (argct == 1 && tmpargs[1][0] == '\0') {argct = 0;}
@@ -486,7 +468,6 @@ int runcmd() {
     if (debug) printf("mkargs:\n");
     if (!mkargs()) goto cmderr;
     if (debug) printf("run commands:\n");
-    //cerr = 255;
     if (debug) printf("C [%d]: {%s}\n", 0, arg[0]);
     for (int i = 1; i <= argct; i++) {
         if (debug) printf("A [%d]: {%s}\n", i, arg[i]);
